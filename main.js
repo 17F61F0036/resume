f7a7510c60a185a585f6283786b02b3e4fa91b61
student <thiru>
2018-07-31 15:53:16 +0530

    Thiru

---
+++ main.js
@@ -0,0 +1,31 @@
+// function loadJSON(file,callback)
+//   var xhr=new XMLHttpRequest();
+//   xhr.overrideMimeType("application/json");
+//   xhr.open("GET",file,true);
+//   xhr.onreadystatechange===function(){
+//     if(xhr.readystate===4 && xhr.status=="200"){
+//       callback(xhr.responceText);
+//     }
+//   }
+//   xhr.send();
+// }
+//  loadJSON("data.json",function(text){
+//    var data=JSON.parse(text);
+//    console.log(data);
+//  }
+function loadjson(file){
+  return new Promise((resolve,reject)=>{
+  return fetch(file).then(responce=>{
+    if(responce.ok){
+      resolve(responce.json());
+    }else{
+      reject(new Error('error'));
+    }
+
+  })
+  })
+}
+var fetchedData=loadjson("data.json");
+fetchedData.then(data=>{
+  console.log(data);
+})
