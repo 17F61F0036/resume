ef8607252bb225efd99ed497556cabc78a11abfc
student <thiru>
2018-08-01 13:10:03 +0530

    Thiru

--- main.js
+++ main.js
@@ -15,9 +15,9 @@
 //  }
 function loadjson(file){
   return new Promise((resolve,reject)=>{
-  return fetch(file).then(responce=>{
-    if(responce.ok){
-      resolve(responce.json());
+  return fetch(file).then(response=>{
+    if(response.ok){
+      resolve(response.json());
     }else{
       reject(new Error('error'));
     }
@@ -25,14 +25,20 @@ function loadjson(file){
   })
   })
 }
+
 var fetchedData=loadjson("data.json");
+//career
 fetchedData.then(data=>{
   console.log(data);
   career(data.career);
+  education(data.education);
+  skills(data.skills);
 })
+
+//Career
 var child2=document.querySelector("#child2");
 function career(car){
-  var heading=document.createElement("heading");
+  var heading=document.createElement("h1");
   heading.textContent="Career Objective";
   child2.appendChild(heading);
   var hr=document.createElement("hr");
@@ -40,5 +46,45 @@ function career(car){
   var p=document.createElement("p");
   p.textContent=car.info;
   child2.appendChild(p);
+}
+  //education
 
+  function education(edu){
+    var heading=document.createElement("h1");
+    heading.textContent="Educational Qualifications";
+    child2.appendChild(heading);
+    var hr=document.createElement("hr");
+    heading.appendChild(hr);
+    var table=document.createElement("table");
+    child2.appendChild(table);
+    var tr="<tr> <td> S.No </td> <td> degree </td> <td> insitute </td> <td> data </td></tr>";
+    //table.innerHTML=tr;
+    table.border="3";
+    var tr1="";
+    for(var i=0;i<edu.length;i++){
+      tr1+="<tr> <td> "+(i+1)+" </td> <td>"+edu[i].degree+"</td> <td> "+edu[i].institute+"</td> <td>"+edu[i].data+"</td> </tr>";
+    }
+    table.innerHTML=tr+tr1;
+}
+
+//skills
+function skills(skill){
+  var heading=document.createElement("h1");
+  heading.textContent="Technical Skills";
+  child2.appendChild(heading);
+  var hr=document.createElement("hr");
+  heading.appendChild(hr);
+  for(var i=0;i<skill.length;i++){
+  var title=document.createElement("h4");
+  title.textContent=skill[i].title;
+  child2.appendChild(title);
+  var list=document.createElement("ul");
+  child2.appendChild(list);
+  console.log(skill[i].set.length);
+  for(var j=0;j<skill[i].set.length;j++){
+    var listItem=document.createElement("li");
+    listItem.textContent=skill[i].set[j];
+    list.appendChild(listItem);
+  }
+}
 }
